f7008d561707d737d04a1623ae83b48c96922306
RomanSavushkin <romarsa82@gmail.com>
2023-09-07 02:05:17 +0300

    9.5

---
+++ src/telran/interviews/MyStack.java
@@ -0,0 +1,42 @@
+package telran.interviews;
+
+import java.util.*;
+//Requirement: all methods must have complexity O[1]
+public class MyStack<T> {
+	LinkedList<T> values = new LinkedList<>();
+	LinkedList<T> maxValues = new LinkedList<>();
+	Comparator<T> comp;
+	public MyStack(Comparator<T> comp) {
+		this.comp = comp;
+	}
+	@SuppressWarnings("unchecked")
+	public MyStack() {
+		this((Comparator<T>)Comparator.naturalOrder());
+	}
+	
+  public void push(T element) {
+	  values.add(element);
+	  if(maxValues.isEmpty() || comp.compare(element,maxValues.getLast()) >= 0) {
+		  maxValues.add(element);
+	  }
+	  
+  }
+  public T pop() {
+	  //TODO removes the stack's top element and returns it out
+	  //In the case no elements exist in the stack the method throws exception NoSuchElementException
+	  T element = values.removeLast();
+	  if (comp.compare(element, maxValues.getLast()) == 0) {
+		  maxValues.removeLast();
+	  }
+	  return element;
+  }
+  public boolean isEmpty() {
+	  
+	  return values.isEmpty();
+  }
+  public T getMax() {
+	  //TODO returns maximal element from the stack
+	  //In the case no elements exist in the stack the method throws exception NoSuchElementException
+	  return maxValues.getLast();
+  }
+}
---
+++ src/telran/interviews/test/MyStackTest.java
@@ -0,0 +1,68 @@
+package telran.interviews.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import telran.interviews.MyStack;
+
+class MyStackTest {
+	int ar[]= {100_000,50_000,100_000,20,20,20,2_000_000, 2_000_000};
+	MyStack<Integer> myStack;
+	MyStack<String> emptyStack = new MyStack<>();
+	@BeforeEach
+	void setUp() throws Exception {
+		myStack = new MyStack<>();
+		fillStack(myStack);
+	}
+
+
+
+	private void fillStack(MyStack<Integer> stack) {
+		for(int num: ar) {
+			stack.push(num);
+		}
+	}
+
+	
+
+	@Test
+	void testPop() {
+		assertEquals(2_000_000, myStack.pop());
+		assertEquals(2_000_000, myStack.pop());
+		assertThrowsExactly(NoSuchElementException.class, () -> emptyStack.pop());
+		
+	}
+
+	@Test
+	void testIsEmpty() {
+		assertFalse(myStack.isEmpty());
+		assertTrue(emptyStack.isEmpty());
+	}
+
+	@Test
+	void testGetMax() {
+		assertEquals(2_000_000, myStack.getMax());
+		myStack.pop();
+		assertEquals(2_000_000, myStack.getMax());
+		myStack.pop();
+		assertEquals(100_000, myStack.getMax());
+		myStack.push(100);
+		assertEquals(100_000, myStack.getMax());
+		myStack.push(1_000_000);
+		assertEquals(1_000_000, myStack.getMax());
+		assertThrowsExactly(NoSuchElementException.class, () -> emptyStack.getMax());
+		
+		
+	}
+	@Test
+	void testComparator() {
+		MyStack<Integer> reversedStack = new MyStack<>((a, b) -> Integer.compare(b, a));
+		fillStack(reversedStack);
+		assertEquals(20, reversedStack.getMax());
+	}
+
+}
