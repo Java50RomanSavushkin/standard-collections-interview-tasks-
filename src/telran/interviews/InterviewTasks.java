95950f469917e34fc85b1490b28cc855710b8ef2
RomanSavushkin <romarsa82@gmail.com>
2023-09-09 23:32:18 +0300

    1

--- src/telran/interviews/InterviewTasks.java
+++ src/telran/interviews/InterviewTasks.java
@@ -1,19 +1,31 @@
 package telran.interviews;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
 
 public class InterviewTasks {
-public static void displayShuffled(int []array) {
-	new Random().ints(0, array.length).distinct().limit(array.length).forEach(i -> System.out.print(array[i] + " "));
-}
-static public List<DateRole> rolesInDates(List<DateRole> datesRoles, List<LocalDate> dates) {
-	TreeMap<LocalDate, String> mapDateRole = new TreeMap<>();
-	datesRoles.forEach(dr -> mapDateRole.put(dr.date(), dr.role()));
-	return dates.stream().map(d -> {
-		Map.Entry<LocalDate, String> entry = mapDateRole.floorEntry(d);
-		DateRole dr = new DateRole(d, entry == null ? null : entry.getValue());
-		return dr;
-	}).toList();
-}
-}
+	public static void displayShuffled(int[] array) {
+		new Random()
+			.ints(0, array.length)
+			.distinct()
+			.limit(array.length)
+			.forEach(i -> System.out.print(array[i] + " "));
+	}
+   
+	static public List<DateRole> rolesInDates(List<DateRole> datesRoles, List<LocalDate> dates) {
+		TreeMap<LocalDate, String> mapDateRole = new TreeMap<>();
+		datesRoles.forEach(dr -> mapDateRole.put(dr.date(), dr.role()));
+		return dates
+				.stream()
+				.map(
+						d -> {Map.Entry<LocalDate, String> entry = mapDateRole.floorEntry(d);
+						DateRole dr = new DateRole(d, entry == null ? null : entry.getValue());
+						return dr;}
+					)
+				.toList(); 
+	}   
+} 
+        
